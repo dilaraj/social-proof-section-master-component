9ce655108a2b4fe3c27dbd67d0341d2549036cdd
dilaraj <dilara.ajaj369@gmail.com>
2026-05-16 21:00:27 +1000

    Completing desktop media queries

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import bgPatternMobileTop from './assets/images/bg-pattern-top-mobile.svg'
 import bgPatternMobileBtm from './assets/images/bg-pattern-bottom-mobile.svg'
+import bgPatternDesktopTop from './assets/images/bg-pattern-top-desktop.svg'
+import bgPatternDesktopBtm from './assets/images/bg-pattern-bottom-desktop.svg'
 import annePrfImg from './assets/images/image-anne.jpg'
 import coltonPrfImg from './assets/images/image-colton.jpg'
 import irenePrfImg from './assets/images/image-irene.jpg'
@@ -13,40 +15,61 @@ import ReviewBox from './components/ReviewBox/ReviewBox'
 export default function App() {
   return (
     <div className="social-proof-section-main">
-      <div className="background-pattern-top"><img src={bgPatternMobileTop} alt="Background Image" className="background-img-top" /></div>
-      <div className="background-pattern-btm"><img src={bgPatternMobileBtm} alt="Background Image" className="background-img-btm" /></div>
+      <div className="background-pattern-top">
+        <img src={bgPatternMobileTop} alt="Background Image" className="background-img-top mobile-img" />
+        <img src={bgPatternDesktopTop} alt="Background Image"  className='background-img-top desktop-img'/>
+      </div>
+      <div className="background-pattern-btm">
+        <img src={bgPatternMobileBtm} alt="Background Image" className="background-img-btm mobile-img" />
+        <img src={bgPatternDesktopBtm} alt="Background Image" className="background-img-btm desktop-img" />
+      </div>
       <div className="social-proof-content">
-        <Hero />
+        <div className="hero-main-container">
+          <Hero />
+        </div>
         <div className="rating-boxes-container">
           <RatingBox 
             rating={5}
             ratingCategory={'Reviews'}
+            marginLeft={'0'}
           />
-          <RatingBox 
-            rating={5}
-            ratingCategory={'Report Guru'}
-          />
-          <RatingBox 
-            rating={5}
-            ratingCategory={'BestTech'}
-          />
+          <div className="second-rating-box">
+            <RatingBox 
+              rating={5}
+              ratingCategory={'Report Guru'}
+              marginLeft={'2em'}
+            />
+          </div>
+          <div className="third-rating-box">
+            <RatingBox 
+              rating={5}
+              ratingCategory={'BestTech'}
+              marginLeft={'4em'}
+            />
+          </div>
         </div>
         <div className="review-boxes-container">
-          <ReviewBox 
-            reviewImg={coltonPrfImg}
-            reviewerName={'Colton Smith'}
-            reviewText={'"We needed the same printered design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!"'}
-          />
-          <ReviewBox 
-            reviewImg={irenePrfImg}
-            reviewerName={'Irene Roberts'}
-            reviewText={'"Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery"'}
-          />
-          <ReviewBox 
-            reviewImg={annePrfImg}
-            reviewerName={'Anne Wallace'}
-            reviewText={'"Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!"'}
-          />
+          <div>
+            <ReviewBox 
+              reviewImg={coltonPrfImg}
+              reviewerName={'Colton Smith'}
+              reviewText={'"We needed the same printered design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!"'}
+            />
+          </div>
+          <div className="second-review-box">
+            <ReviewBox 
+              reviewImg={irenePrfImg}
+              reviewerName={'Irene Roberts'}
+              reviewText={'"Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery"'}
+            />
+          </div>
+          <div className="third-review-box">
+            <ReviewBox 
+              reviewImg={annePrfImg}
+              reviewerName={'Anne Wallace'}
+              reviewText={'"Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!"'}
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import './Hero.css'
 export default function Hero() {
     return (
         <div className="hero-container">
-            <div className="hero-header">10,000+ of our users love our products</div>
+            <div className="hero-header">10,000+ of our users love our products.</div>
             <div className="hero-text">
                 We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services
             </div>
